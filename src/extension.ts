d661bd98df7262a9da60c132a44ceba2a1599998
Gavin Barnett <gavbarnett@gmail.com>
2022-05-31 23:45:17 +0100

    Adding white chess piece
    This is just a test that the pieces can be correctly scaled, positioned and rendered.
    SVG data is attributed to:
    Cburnett, CC BY-SA 3.0 <http://creativecommons.org/licenses/by-sa/3.0/>, via Wikimedia Commons

--- src/extension.ts
+++ src/extension.ts
@@ -204,12 +204,100 @@ function generateBoard(boardGrid: [string[]])
 	var boardHeight: number = boardGrid.length;
 	var returnHtml: string =
 	`<rect `+
-		`x="100" y="50 "`+
+		`x="` + gridSize.toString() + `" y="` + (gridSize/2).toString() + `" `+
 		`width="` + (gridSize*boardWidth).toString() + `" `+
 		`height="` + (gridSize*boardHeight).toString() + `" `+
 		`fill="OliveDrab"`+
 	`/> `;
-	
+	//Add chess pieces
+	returnHtml += `<symbol id="chesswhiteking" width="100" height ="100">
+	<g transform="scale(2.22)" style="fill:none; fill-opacity:1; fill-rule:evenodd; stroke:#000000; stroke-width:1.5; stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4; stroke-dasharray:none; stroke-opacity:1;">
+		<path d="M 22.5,11.63 L 22.5,6" style="fill:none; stroke:#000000; stroke-linejoin:miter;"/>
+		<path d="M 20,8 L 25,8" style="fill:none; stroke:#000000; stroke-linejoin:miter;"/>
+		<path d="M 22.5,25 C 22.5,25 27,17.5 25.5,14.5 C 25.5,14.5 24.5,12 22.5,12 C 20.5,12 19.5,14.5 19.5,14.5 C 18,17.5 22.5,25 22.5,25" style="fill:#ffffff; stroke:#000000; stroke-linecap:butt; stroke-linejoin:miter;"/>
+		<path d="M 12.5,37 C 18,40.5 27,40.5 32.5,37 L 32.5,30 C 32.5,30 41.5,25.5 38.5,19.5 C 34.5,13 25,16 22.5,23.5 L 22.5,27 L 22.5,23.5 C 20,16 10.5,13 6.5,19.5 C 3.5,25.5 12.5,30 12.5,30 L 12.5,37" style="fill:#ffffff; stroke:#000000;"/>
+		<path d="M 12.5,30 C 18,27 27,27 32.5,30" style="fill:none; stroke:#000000;"/>
+		<path d="M 12.5,33.5 C 18,30.5 27,30.5 32.5,33.5" style="fill:none; stroke:#000000;"/>
+		<path d="M 12.5,37 C 18,34 27,34 32.5,37" style="fill:none; stroke:#000000;"/>
+	</g></symbol>`;
+	returnHtml += `<symbol id="chesswhitequeen" width="100" height ="100">
+	<g transform="scale(2.22)" style="fill:#ffffff;stroke:#000000;stroke-width:1.5;stroke-linejoin:round">
+		<path d="M 9,26 C 17.5,24.5 30,24.5 36,26 L 38.5,13.5 L 31,25 L 30.7,10.9 L 25.5,24.5 L 22.5,10 L 19.5,24.5 L 14.3,10.9 L 14,25 L 6.5,13.5 L 9,26 z"/>
+		<path d="M 9,26 C 9,28 10.5,28 11.5,30 C 12.5,31.5 12.5,31 12,33.5 C 10.5,34.5 11,36 11,36 C 9.5,37.5 11,38.5 11,38.5 C 17.5,39.5 27.5,39.5 34,38.5 C 34,38.5 35.5,37.5 34,36 C 34,36 34.5,34.5 33,33.5 C 32.5,31 32.5,31.5 33.5,30 C 34.5,28 36,28 36,26 C 27.5,24.5 17.5,24.5 9,26 z"/>
+		<path d="M 11.5,30 C 15,29 30,29 33.5,30" style="fill:none"/>
+		<path d="M 12,33.5 C 18,32.5 27,32.5 33,33.5" style="fill:none"/>
+		<circle cx="6" cy="12" r="2" />
+		<circle cx="14" cy="9" r="2" />
+		<circle cx="22.5" cy="8" r="2" />
+		<circle cx="31" cy="9" r="2" />
+		<circle cx="39" cy="12" r="2" />
+	</g></symbol>`;
+	returnHtml += `<symbol id="chesswhiterook" width="100" height ="100">
+	<g transform="scale(2.22)" style="opacity:1; fill:#ffffff; fill-opacity:1; fill-rule:evenodd; stroke:#000000; stroke-width:1.5; stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4; stroke-dasharray:none; stroke-opacity:1;" transform="translate(0,0.3)">
+	<path
+		d="M 9,39 L 36,39 L 36,36 L 9,36 L 9,39 z "
+		style="stroke-linecap:butt;" />
+	<path
+		d="M 12,36 L 12,32 L 33,32 L 33,36 L 12,36 z "
+		style="stroke-linecap:butt;" />
+	<path
+		d="M 11,14 L 11,9 L 15,9 L 15,11 L 20,11 L 20,9 L 25,9 L 25,11 L 30,11 L 30,9 L 34,9 L 34,14"
+		style="stroke-linecap:butt;" />
+	<path
+		d="M 34,14 L 31,17 L 14,17 L 11,14" />
+	<path
+		d="M 31,17 L 31,29.5 L 14,29.5 L 14,17"
+		style="stroke-linecap:butt; stroke-linejoin:miter;" />
+	<path
+		d="M 31,29.5 L 32.5,32 L 12.5,32 L 14,29.5" />
+	<path
+		d="M 11,14 L 34,14"
+		style="fill:none; stroke:#000000; stroke-linejoin:miter;" />
+	</g></symbol>`;
+	returnHtml += `<symbol id="chesswhiteking" width="100" height ="100">
+	<g transform="scale(2.22)" style="fill:none; fill-opacity:1; fill-rule:evenodd; stroke:#000000; stroke-width:1.5; stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4; stroke-dasharray:none; stroke-opacity:1;">
+		<path d="M 22.5,11.63 L 22.5,6" style="fill:none; stroke:#000000; stroke-linejoin:miter;"/>
+		<path d="M 20,8 L 25,8" style="fill:none; stroke:#000000; stroke-linejoin:miter;"/>
+		<path d="M 22.5,25 C 22.5,25 27,17.5 25.5,14.5 C 25.5,14.5 24.5,12 22.5,12 C 20.5,12 19.5,14.5 19.5,14.5 C 18,17.5 22.5,25 22.5,25" style="fill:#ffffff; stroke:#000000; stroke-linecap:butt; stroke-linejoin:miter;"/>
+		<path d="M 12.5,37 C 18,40.5 27,40.5 32.5,37 L 32.5,30 C 32.5,30 41.5,25.5 38.5,19.5 C 34.5,13 25,16 22.5,23.5 L 22.5,27 L 22.5,23.5 C 20,16 10.5,13 6.5,19.5 C 3.5,25.5 12.5,30 12.5,30 L 12.5,37" style="fill:#ffffff; stroke:#000000;"/>
+		<path d="M 12.5,30 C 18,27 27,27 32.5,30" style="fill:none; stroke:#000000;"/>
+		<path d="M 12.5,33.5 C 18,30.5 27,30.5 32.5,33.5" style="fill:none; stroke:#000000;"/>
+		<path d="M 12.5,37 C 18,34 27,34 32.5,37" style="fill:none; stroke:#000000;"/>
+	</g></symbol>`;
+	returnHtml += `<symbol id="chesswhitebishop" width="100" height ="100">
+	<g transform="scale(2.22)" style="opacity:1; fill:none; fill-rule:evenodd; fill-opacity:1; stroke:#000000; stroke-width:1.5; stroke-linecap:round; stroke-linejoin:round; stroke-miterlimit:4; stroke-dasharray:none; stroke-opacity:1;" transform="translate(0,0.6)">>
+		<g style="fill:#ffffff; stroke:#000000; stroke-linecap:butt;">
+			<path d="M 9,36 C 12.39,35.03 19.11,36.43 22.5,34 C 25.89,36.43 32.61,35.03 36,36 C 36,36 37.65,36.54 39,38 C 38.32,38.97 37.35,38.99 36,38.5 C 32.61,37.53 25.89,38.96 22.5,37.5 C 19.11,38.96 12.39,37.53 9,38.5 C 7.65,38.99 6.68,38.97 6,38 C 7.35,36.54 9,36 9,36 z"/>
+			<path d="M 15,32 C 17.5,34.5 27.5,34.5 30,32 C 30.5,30.5 30,30 30,30 C 30,27.5 27.5,26 27.5,26 C 33,24.5 33.5,14.5 22.5,10.5 C 11.5,14.5 12,24.5 17.5,26 C 17.5,26 15,27.5 15,30 C 15,30 14.5,30.5 15,32 z"/>
+			<path d="M 25 8 A 2.5 2.5 0 1 1  20,8 A 2.5 2.5 0 1 1  25 8 z"/>
+		</g>
+	<path d="M 17.5,26 L 27.5,26 M 15,30 L 30,30 M 22.5,15.5 L 22.5,20.5 M 20,18 L 25,18" style="fill:none; stroke:#000000; stroke-linejoin:miter;"/>
+	</g></symbol>`;
+	returnHtml += `<symbol id="chesswhiteknight" width="100" height ="100">
+	<g transform="scale(2.22)" style="opacity:1; fill:none; fill-opacity:1; fill-rule:evenodd; stroke:#000000; stroke-width:1.5; stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4; stroke-dasharray:none; stroke-opacity:1;" transform="translate(0,0.3)">
+	<path
+		d="M 22,10 C 32.5,11 38.5,18 38,39 L 15,39 C 15,30 25,32.5 23,18"
+		style="fill:#ffffff; stroke:#000000;" />
+	<path
+		d="M 24,18 C 24.38,20.91 18.45,25.37 16,27 C 13,29 13.18,31.34 11,31 C 9.958,30.06 12.41,27.96 11,28 C 10,28 11.19,29.23 10,30 C 9,30 5.997,31 6,26 C 6,24 12,14 12,14 C 12,14 13.89,12.1 14,10.5 C 13.27,9.506 13.5,8.5 13.5,7.5 C 14.5,6.5 16.5,10 16.5,10 L 18.5,10 C 18.5,10 19.28,8.008 21,7 C 22,7 22,10 22,10"
+		style="fill:#ffffff; stroke:#000000;" />
+	<path
+		d="M 9.5 25.5 A 0.5 0.5 0 1 1 8.5,25.5 A 0.5 0.5 0 1 1 9.5 25.5 z"
+		style="fill:#000000; stroke:#000000;" />
+	<path
+		d="M 15 15.5 A 0.5 1.5 0 1 1  14,15.5 A 0.5 1.5 0 1 1  15 15.5 z"
+		transform="matrix(0.866,0.5,-0.5,0.866,9.693,-5.173)"
+		style="fill:#000000; stroke:#000000;" />
+	</g></symbol>`;
+	returnHtml += `<symbol id="chesswhitepawn" width="100" height ="100">
+	<g transform="scale(2.22)">
+	<path 
+		d="m 22.5,9 c -2.21,0 -4,1.79 -4,4 0,0.89 0.29,1.71 0.78,2.38 C 17.33,16.5 16,18.59 16,21 c 0,2.03 0.94,3.84 2.41,5.03 C 15.41,27.09 11,31.58 11,39.5 H 34 C 34,31.58 29.59,27.09 26.59,26.03 28.06,24.84 29,23.03 29,21 29,18.59 27.67,16.5 25.72,15.38 26.21,14.71 26.5,13.89 26.5,13 c 0,-2.21 -1.79,-4 -4,-4 z" 
+		style="opacity:1; 
+		fill:#ffffff; fill-opacity:1; fill-rule:nonzero; 
+		stroke:#000000; stroke-width:1.5; stroke-linecap:round; stroke-linejoin:miter; stroke-miterlimit:4; stroke-dasharray:none; stroke-opacity:1;"/>
+	</g></symbol>`;
+
 	returnHtml += `<path d="`;
 	var offsetType = (boardWidth % 2 === boardHeight % 2);
 	for (var h = 0; h < boardHeight ;h++)
@@ -231,6 +319,26 @@ function generateBoard(boardGrid: [string[]])
 	`stroke-dasharray="` + gridSize + `" ` +
 	`stroke-width="` + gridSize + `" ` +
 	`/>`;
+	returnHtml +=`<use x="100" y="750" 	xlink:href="#chesswhiterook"/>`;
+	returnHtml +=`<use x="200" y="750" 	xlink:href="#chesswhitebishop"/>`;
+	returnHtml +=`<use x="300" y="750" 	xlink:href="#chesswhiteknight"/>`;
+	returnHtml +=`<use x="400" y="750" 	xlink:href="#chesswhiteking"/>`;
+	returnHtml +=`<use x="500" y="750" 	xlink:href="#chesswhitequeen"/>`;
+	returnHtml +=`<use x="600" y="750" 	xlink:href="#chesswhiteknight"/>`;
+	returnHtml +=`<use x="700" y="750" 	xlink:href="#chesswhitebishop"/>`;
+	returnHtml +=`<use x="800" y="750" 	xlink:href="#chesswhiterook"/>`;
+
+	returnHtml +=`<use x="100" y="650" 	xlink:href="#chesswhitepawn"/>`;
+	returnHtml +=`<use x="200" y="650" 	xlink:href="#chesswhitepawn"/>`;
+	returnHtml +=`<use x="300" y="650" 	xlink:href="#chesswhitepawn"/>`;
+	returnHtml +=`<use x="400" y="650" 	xlink:href="#chesswhitepawn"/>`;
+	returnHtml +=`<use x="500" y="650" 	xlink:href="#chesswhitepawn"/>`;
+	returnHtml +=`<use x="600" y="650" 	xlink:href="#chesswhitepawn"/>`;
+	returnHtml +=`<use x="700" y="650" 	xlink:href="#chesswhitepawn"/>`;
+	returnHtml +=`<use x="800" y="650" 	xlink:href="#chesswhitepawn"/>`;
+
+
+
 	console.log(returnHtml);
 	return(returnHtml);
 }
